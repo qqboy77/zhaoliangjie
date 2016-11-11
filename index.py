ff8b92873ab1c93cfa6283dcd09c527be38de216
赵琦 <zhao7sky@126.com>
2016-11-11 14:55:47 +0800

    1

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return '<h1>赵良杰</h1><a href="http://baike.baidu.com/link?url=e_3-EEOhHoqu45yvU589kVuYR6NcZsF1JjebZpEfz4W6n8GcIUVQ6LXsgxYu7bHIQQroT_lA6ps7Jtx8gIAb1k6mWjhQT_UEL7DuplBdr93">baiji</a>'
+	return '<h1>赵良杰药材种植</h1><a href="http://baike.baidu.com/link?url=e_3-EEOhHoqu45yvU589kVuYR6NcZsF1JjebZpEfz4W6n8GcIUVQ6LXsgxYu7bHIQQroT_lA6ps7Jtx8gIAb1k6mWjhQT_UEL7DuplBdr93">白芨相关知识</a>'
 
 
 if __name__ == '__main__':
---
+++ untitled.py
@@ -0,0 +1,10 @@
+from flask import flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return '<h1>赵良杰</h1><a href="http://baike.baidu.com/link?url=e_3-EEOhHoqu45yvU589kVuYR6NcZsF1JjebZpEfz4W6n8GcIUVQ6LXsgxYu7bHIQQroT_lA6ps7Jtx8gIAb1k6mWjhQT_UEL7DuplBdr93">baiji</a>'
+
+
+if __name__ == '__main__':
+		app.run(debug==True)
